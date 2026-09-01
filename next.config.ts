a6fb7db1441d4a6d54902a28e49ec29bcee114ae
Gaurav Kumar <33researchlabs@gmail.com>
2026-09-01 14:52:52 +0530

    fixed and update

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  serverExternalPackages: [
-    "@coral-xyz/anchor",
-    "@pump-fun/pump-sdk",
-    "@pump-fun/pump-swap-sdk",
-  ],
 };
 
 export default nextConfig;
